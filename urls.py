2aa522c05ce48a5dcbbc5561c6902ad0a16386d0
senf-f <mate.mrse@gmail.com>
2025-02-07 10:16:56 +0100

    Update urls.py

--- urls.py
+++ urls.py
@@ -11,4 +11,5 @@ urls = ["https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/9834527f-650f-4c66-
         "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/4204d946-1e4b-499a-998f-39d2fa60890d", 
         "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/2545891a-135b-43a3-a0ed-fd6c33874908",
         "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/bc627247-2915-419f-a90c-e08bf197298a", 
-        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/f704d27f-a129-4bb9-8c8f-6b5340bcd131"]
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/f704d27f-a129-4bb9-8c8f-6b5340bcd131", 
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/a01e48ea-7873-4076-9baa-0d5cdd832e18"]
