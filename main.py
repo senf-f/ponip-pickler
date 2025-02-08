e9d4aba32b9c1c26e8dbbc92e400e1feb6300cb2
mate.mrse <mate.mrse@gmail.com>
2025-02-08 18:59:06 +0100

    Refactoring: cleanup print statements

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def write_sales_info(session, data):
 
     # Check if the record already exists
     existing_record = session.query(SalesInfo).filter_by(id=data["ID nadmetanja"]).first()
-    print(f"MM {data=}")
+
     if existing_record:
         trenutna_cijena_key = 'Trenutačna cijena predmeta prodaje u\xa0nadmetanju'
         logging.debug(f"Updating existing record for ID {data['ID nadmetanja']}.")
@@ -102,8 +102,7 @@ def write_sales_info(session, data):
         existing_record.data_hash = data_hash
         existing_record.json_data = json_data
         logging.info(f"Updated sales info for ID {data['ID nadmetanja']}.")
-        print(f"MM {existing_record.iznos_najvise_ponude=}")
-        print(f"MM {existing_record.broj_uplatitelja=}")
+
     else:
         logging.debug(f"Creating new record for ID {data['ID nadmetanja']}.")
         new_record = SalesInfo(
@@ -166,7 +165,6 @@ def process_urls(session):
         try:
             raw_html = get_html(url)
             data = parse_html(raw_html)
-            print(f"*MM {data=}")
 
             logging.debug(f"Parsed data: {data}")
 
