119e2a035abb8831663360a556867c35d9aa8fb7
senf-f <mate.mrse@gmail.com>
2026-04-08 12:54:47 +0200

    Add new URL to urls.py and duplicate existing one
    U istoj kuci je bio buffet K Vilimu

--- urls.py
+++ urls.py
@@ -1,2 +1,3 @@
 urls = ["https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/0151526b-067c-4cb6-9869-0db2a78e2122",
-        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/e35541a8-1149-4d31-abb3-dbde55b2ba70"]
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/e35541a8-1149-4d31-abb3-dbde55b2ba70", 
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/de1d14ab-9235-4ee7-9ce5-81bb4251771f"]
