3f73f2dde3f38eb07fcd4fcb01ca9787e32b8dfb
senf-f <mate.mrse@gmail.com>
2026-04-08 12:56:50 +0200

    Add new URLs to the list in urls.py

--- urls.py
+++ urls.py
@@ -1,3 +1,4 @@
 urls = ["https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/0151526b-067c-4cb6-9869-0db2a78e2122",
         "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/e35541a8-1149-4d31-abb3-dbde55b2ba70", 
-        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/de1d14ab-9235-4ee7-9ce5-81bb4251771f"]
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/de1d14ab-9235-4ee7-9ce5-81bb4251771f",
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/331abd97-da7c-48d6-b9a6-6eb8dc1761c1"]
