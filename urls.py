63163ed5a438de5b8a1ed814670b40c5ed6d88f5
senf-f <mate.mrse@gmail.com>
2026-04-08 12:52:43 +0200

    Update URLs in urls.py

--- urls.py
+++ urls.py
@@ -1,2 +1,2 @@
 urls = ["https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/0151526b-067c-4cb6-9869-0db2a78e2122",
-        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/6a022e71-3599-4b19-b4e4-c9efeb03498a"]
+        "https://ponip.fina.hr/ocevidnik-web/predmet_prodaje/e35541a8-1149-4d31-abb3-dbde55b2ba70"]
